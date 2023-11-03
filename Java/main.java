34977d2337fc46b23daef86eb44119582f6f2ac7
Pioo157 <saulperalta01@gmail.com>
2023-11-02 20:50:16 -0600

    Agregando libreria

--- Java/main.java
+++ Java/main.java
@@ -1,5 +1,8 @@
+import math.mat;
+
 class main{
 	public static void main(String[] args){
-		System.out.println("Hola como estan");
+		System.out.println("Hola como estan \n");
+		System.out.println(mat.suma(mat.suma(1,2),3));
 	}
 }
---
+++ Java/math/mat.java
@@ -0,0 +1,7 @@
+package math;
+public class mat{
+	public static int suma(int a, int b){
+		return a+b;
+	}
+}
+
