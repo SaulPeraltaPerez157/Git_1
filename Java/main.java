70c0f3f4a4ca2ccc66c959dacf46a411b5a2e276
Pioo157 <saulperalta01@gmail.com>
2023-11-18 08:34:08 -0600

    Agegando archivo hellogit3.py

--- Java/main.java
+++ Java/main.java
@@ -4,5 +4,11 @@ class main{
 	public static void main(String[] args){
 		System.out.println("Hola como estan \n");
 		System.out.println(mat.suma(mat.suma(1,2),3));
+		if(mat.aprobado(8)==0){
+			System.out.println("No aprobado");
+		}
+		else{
+			System.out.println("Aprobado");
+		}
 	}
 }
--- Java/math/mat.java
+++ Java/math/mat.java
@@ -3,5 +3,14 @@ public class mat{
 	public static int suma(int a, int b){
 		return a+b;
 	}
+
+	public static int aprobado(int a){
+		if(a<6){
+			return 0;
+		}
+		else{
+			return 1;
+		}
+	}
 }
 
